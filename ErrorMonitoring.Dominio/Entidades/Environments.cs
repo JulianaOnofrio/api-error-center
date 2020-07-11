93b24337c6b3392753378048dde080deb2cd3cd8
JulianaOnofrio <57050340+JulianaOnofrio@users.noreply.github.com>
2020-07-10 22:47:14 -0300

    Api central de erros

---
+++ ErrorMonitoring.Dominio/Entidades/Environments.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ErrorMonitoring.Dominio.Entidades
+{
+    public class Environments
+    {
+        public Environments()
+        {
+            ProjectsEnvironments = new HashSet<ProjectsEnvironments>();
+
+        
+        }
+
+        public int Id { get; set; }
+        public string EName { get; set; }
+
+        public virtual ICollection<ProjectsEnvironments> ProjectsEnvironments { get; set; }
+    }
+}
